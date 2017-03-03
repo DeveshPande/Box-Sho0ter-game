0b0038345c8e85ab8ab49ff2b921ad82576e2561
DeveshPande <deveshp04@gmail.com>
2017-03-02 23:16:26 -0500

    unity 3d project
    first person game

---
+++ game/Assets/Scripts/BasicMover.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicMover : MonoBehaviour {
+
+
+    public float spinSpeed = 140.0f;
+    public float motionMagnitude = 0.1f;
+    public bool rotate=true;
+    public bool move=true;
+    
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (rotate)
+        {
+            //for rotation
+            
+            gameObject.transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
+        }
+	   //for moving
+        if (move)
+        {
+            gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+        }
+	}
+}
